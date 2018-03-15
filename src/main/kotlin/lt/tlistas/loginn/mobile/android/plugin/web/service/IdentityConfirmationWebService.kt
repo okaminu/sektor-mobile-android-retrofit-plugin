7d76e122a3bc29fcb8d883befa4c5cfa67876ca2
Okaminu <aurimasdgt@gmail.com>
2018-03-15 17:12:30 +0200

    authentication changed to identity confirmation

--- src/main/kotlin/lt/tlistas/loginn/mobile/android/plugin/web/service/IdentityConfirmationWebService.kt
+++ src/main/kotlin/lt/tlistas/loginn/mobile/android/plugin/web/service/IdentityConfirmationWebService.kt
@@ -1,15 +1,11 @@
 package lt.tlistas.loginn.mobile.android.plugin.web.service
 
-import retrofit2.Call
-import retrofit2.http.POST
-import retrofit2.http.Path
-
 interface IdentityConfirmationWebService {
 
-    @POST("/collaborator/authentication/code/request/{mobileNumber}")
+    @POST("/collaborator/identity-confirmation/code/request/{mobileNumber}")
     fun requestCode(@Path("mobileNumber") mobileNumber: String): Call<Void>
 
-    @POST("/collaborator/authentication/code/confirm/{code}")
+    @POST("/collaborator/identity-confirmation/code/confirm/{code}")
     fun confirmCode(@Path("code") confirmationCode: String): Call<String>
 
 }
