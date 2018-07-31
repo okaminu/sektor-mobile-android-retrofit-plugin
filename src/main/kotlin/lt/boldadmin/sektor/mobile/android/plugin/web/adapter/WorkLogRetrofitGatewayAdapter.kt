8980b39cc87257a8630525b227360ea8aba3d4d2
Andrius Degutis <andriusdgt@gmail.com>
2018-07-31 15:06:05 +0300

    BA-443-worklog: style changes

--- src/main/kotlin/lt/boldadmin/sektor/mobile/android/plugin/web/adapter/WorkLogRetrofitGatewayAdapter.kt
+++ src/main/kotlin/lt/boldadmin/sektor/mobile/android/plugin/web/adapter/WorkLogRetrofitGatewayAdapter.kt
@@ -5,8 +5,9 @@ import lt.boldadmin.sektor.mobile.android.api.valueobject.GpsCoordinates
 import lt.boldadmin.sektor.mobile.android.plugin.web.factory.RetrofitFactory
 import lt.boldadmin.sektor.mobile.android.plugin.web.service.WorkLogWebService
 
-class WorkLogRetrofitGatewayAdapter(token: String, private val webClient: WorkLogWebService = RetrofitFactory()
-        .create(WorkLogWebService::class.java, token)) : WorkLogGateway {
+class WorkLogRetrofitGatewayAdapter(
+    token: String, private val webClient: WorkLogWebService = RetrofitFactory().create(WorkLogWebService::class.java, token)
+) : WorkLogGateway {
 
     override fun logByLocation(gpsCoordinates: GpsCoordinates) {
         webClient.logByLocation(gpsCoordinates).execute()
