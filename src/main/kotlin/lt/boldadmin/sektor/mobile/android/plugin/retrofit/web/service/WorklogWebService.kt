a766ee729318f3c777957754d1e8c2ab22cb5fc0
Okaminu <aurimasdgt@gmail.com>
2019-02-26 18:26:02 +0200

    code review fixes

--- src/main/kotlin/lt/boldadmin/sektor/mobile/android/plugin/retrofit/web/service/WorklogWebService.kt
+++ src/main/kotlin/lt/boldadmin/sektor/mobile/android/plugin/retrofit/web/service/WorklogWebService.kt
@@ -4,10 +4,7 @@ import lt.boldadmin.sektor.mobile.android.api.type.entity.WorkLogIntervalEndpoin
 import lt.boldadmin.sektor.mobile.android.api.valueobject.GpsCoordinates
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface WorklogWebService {
 
