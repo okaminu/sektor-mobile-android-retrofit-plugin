1d90bcba4f73adb2136a0eaf9c01d9dfe1b71033
vsh1ft <kastytis.ch@gmail.com>
2019-07-22 20:43:07 +0300

    BA-604: detekt linting added

--- src/test/kotlin/lt/boldadmin/sektor/mobile/android/plugin/retrofit/test/unit/web/adapter/ConnectionStatusRetrofitGatewayAdapterTest.kt
+++ src/test/kotlin/lt/boldadmin/sektor/mobile/android/plugin/retrofit/test/unit/web/adapter/ConnectionStatusRetrofitGatewayAdapterTest.kt
@@ -11,7 +11,6 @@ import org.mockito.junit.MockitoJUnitRunner
 import retrofit2.Call
 import retrofit2.Response
 
-
 @RunWith(MockitoJUnitRunner::class)
 class ConnectionStatusRetrofitGatewayAdapterTest {
 
