5d43fc673efefa1ad7024647b635a611e4052ce8
Andrius Degutis <andriusdgt@gmail.com>
2020-02-17 13:26:43 +0200

    BA-891: removed default parameters

--- src/test/kotlin/lt/boldadmin/sektor/mobile/android/plugin/retrofit/test/unit/web/adapter/CollaboratorRetrofitGatewayAdapterTest.kt
+++ src/test/kotlin/lt/boldadmin/sektor/mobile/android/plugin/retrofit/test/unit/web/adapter/CollaboratorRetrofitGatewayAdapterTest.kt
@@ -5,7 +5,9 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.same
 import com.nhaarman.mockitokotlin2.verify
 import lt.boldadmin.sektor.mobile.android.api.valueobject.DayMinuteInterval
+import lt.boldadmin.sektor.mobile.android.api.valueobject.DayOfWeek.TUESDAY
 import lt.boldadmin.sektor.mobile.android.api.valueobject.GpsCoordinates
+import lt.boldadmin.sektor.mobile.android.api.valueobject.MinuteInterval
 import lt.boldadmin.sektor.mobile.android.plugin.retrofit.web.adapter.CollaboratorRetrofitGatewayAdapter
 import lt.boldadmin.sektor.mobile.android.plugin.retrofit.web.service.CollaboratorWebService
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -48,7 +50,7 @@ class CollaboratorRetrofitGatewayAdapterTest {
 
     @Test
     fun `Gets work week`() {
-        val expectedWorkWeek = sortedSetOf(DayMinuteInterval())
+        val expectedWorkWeek = sortedSetOf(DayMinuteInterval(TUESDAY, MinuteInterval(10, 20), false))
         doReturn(callSpy).`when`(collaboratorWebServiceSpy).getWorkWeek()
         doReturn(Response.success(expectedWorkWeek)).`when`(callSpy).execute()
 
